b7dcf6b4e4a21bbeae929b07ad4550fb796d09aa
Kimleng <kimlenghor2006@gmail.com>
2023-08-27 17:26:46 -0500

    learn about routing

---
+++ components/ui/button.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import styles from './button.module.css';
+
+function Button(props) { 
+
+    if (props.link) {
+        return (
+            <Link className={styles.btn} href={props.link}>
+                {props.children}
+            </Link>
+        );
+    }
+
+    return <button className={styles.btn} onClick={props.onClick}>{props.children}</button>
+}
+
+export default Button;
---
+++ pages/events/index.js
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+import { getAllEvents } from "../../dummy-data";
+
+function AllEventsPage() {
+
+    const events = getAllEvents();
+    const router = useRouter();
+
+    function findEventHandler(year, month) { 
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    }
+
+    return (
+        <Fragment>
+            <EventsSearch onSearch={findEventHandler} />
+            <EventList items={events} />
+        </Fragment>
+    )
+}
+
+export default AllEventsPage;
