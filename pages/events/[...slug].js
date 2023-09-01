a72f324e4d4145674d76d9e6fb0565486cac02f0
Kimleng <kimlenghor2006@gmail.com>
2023-09-01 16:17:41 -0500

    add serverside rendering

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,17 +1,38 @@
 import { useRouter } from "next/router";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
-import { getFilteredEvents } from "../../dummy-data";
+// import { getFilteredEvents } from "../../helpers/api-util";
+import useSWR from "swr";
 
 function FilterEventsPage() {
+
+    const [events, setEvents] = useState();
 
     const router = useRouter();
     const filterData = router.query.slug;
 
-    if (!filterData) {
+    const { data, error } = useSWR('https://react-getting-started-2edcf-default-rtdb.firebaseio.com/events.json',
+        (url) => fetch(url).then(res => res.json()));
+
+    useEffect(() => { 
+        if (data) { 
+            const events = [];
+    
+            for (const key in data) { 
+                events.push({
+                    id: key,
+                    ...data[key]
+                });
+            }
+
+            setEvents(events);
+        }
+    }, [data]);
+
+    if (!events) {
         return <p className="center">Loading...</p>
     }
 
@@ -27,12 +48,12 @@ function FilterEventsPage() {
         numYear > 2030 ||
         numYear < 2021 ||
         numMonth < 1 ||
-        numMonth > 12
+        numMonth > 12 || error
     ) { 
         return (
             <Fragment>
                 <ErrorAlert>
-                    <p>Invalid filter. Please adjust your values!</p>;
+                    <p>Invalid filter. Please adjust your values!</p>
                 </ErrorAlert>
                 <div className="center">
                     <Button link='/events'>Show All Events</Button>
@@ -41,9 +62,9 @@ function FilterEventsPage() {
         );
     }
 
-    const filteredEvents = getFilteredEvents({
-        year: numYear,
-        month: numMonth,
+    let filteredEvents = events.filter((event) => {
+        const eventDate = new Date(event.date);
+        return eventDate.getFullYear() === numYear && eventDate.getMonth() === numMonth - 1;
     });
 
     if (!filteredEvents || filteredEvents.length == 0) { 
@@ -59,12 +80,53 @@ function FilterEventsPage() {
         );
     }
 
-    const date = new Date(numYear, numMonth - 1)
+    const date = new Date(numYear.year, numMonth - 1)
 
     return <Fragment>
         <ResultsTitle date={date} />
         <EventList items={filteredEvents} />
     </Fragment>
 }
+
+// export async function getServerSideProps(context) { 
+    
+//     const { params } = context;
+
+//     const filterData = params.slug;
+
+//     const filteredYear = filterData[0]
+//     const filteredMonth = filterData[1]
+
+//     const numYear = +filteredYear;
+//     const numMonth = +filteredMonth;
+
+//     if (
+//         isNaN(numYear) ||
+//         isNaN(numMonth) ||
+//         numYear > 2030 ||
+//         numYear < 2021 ||
+//         numMonth < 1 ||
+//         numMonth > 12
+//     ) { 
+//         return {
+//             props: { hasError: true },
+//         };
+//     }
+
+//     const filteredEvents = await getFilteredEvents({
+//         year: numYear,
+//         month: numMonth,
+//     });
+    
+//     return {
+//         props: {
+//             events: filteredEvents,
+//             date: {
+//                 year: numYear,
+//                 month: numMonth,
+//             }
+//         },
+//     };
+// }
 
 export default FilterEventsPage;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
 import EventList from "../components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 
-function HomePage() { 
-
-    const featuredEvents = getFeaturedEvents();
-
+function HomePage(props) {
     return <div>
         <ul>
-            <EventList items={featuredEvents} />
+            <EventList items={props.events} />
         </ul>
     </div>
+}
+
+export async function getStaticProps() { 
+
+    const featuredEvents = await getFeaturedEvents();
+
+    return {
+        props: {
+            events: featuredEvents,
+            revalidate: 1800,
+        }
+    }
 }
 
 export default HomePage;
